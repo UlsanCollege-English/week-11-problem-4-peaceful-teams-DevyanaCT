ce6acfc6361d71c56effdf7aceafed557ad38d80
DevyanaCT <chandthakuridevyana@gmail.com>
2025-11-27 09:41:47 +0900

    changes4

--- conftest.py
+++
@@ -1,7 +0,0 @@
-"""conftest.py for problem 4 to ensure hw04 module is importable."""
-import sys
-import os
-
-problem_dir = os.path.dirname(os.path.abspath(__file__))
-if problem_dir not in sys.path:
-    sys.path.insert(0, problem_dir)
